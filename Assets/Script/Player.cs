05dc2d47f46b8c2926a16c0bcc00ec38ffe724dc
axel.bentodas <axel.bentodasilva@he-arc.ch>
2018-04-28 18:54:52 +0200

    Add platform to game scene

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
-    private bool active;
+    private bool active = true;
     public bool Active {
         get {
             return active;
