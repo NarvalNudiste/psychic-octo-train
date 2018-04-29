a9cddd8f0ed63fc71d21459ccf018302a31d706f
axel.bentodas <axel.bentodasilva@he-arc.ch>
2018-04-29 11:57:40 +0200

    Add platforms

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+    
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("SombreroGame", LoadSceneMode.Single);
+        }
+    }
+}
