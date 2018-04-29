752bacd9147f06eb4bb4c1150abf4d04be608583
axel.bentodas <axel.bentodasilva@he-arc.ch>
2018-04-29 14:55:50 +0200

    Add reset & Exit game

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -8,9 +8,9 @@ public class GameManager : MonoBehaviour {
 
     // Update is called once per frame
     void FixedUpdate() {
-        if (Input.GetKey(KeyCode.Escape))
-        {
+        if (Input.GetKey(KeyCode.R))
             SceneManager.LoadScene("SombreroGame", LoadSceneMode.Single);
-        }
+        if (Input.GetKey(KeyCode.Escape))
+            SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
     }
 }
