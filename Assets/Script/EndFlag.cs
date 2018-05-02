82948b6b3a9098133f3e41ea5b23cf03a34f96ff
axel.bentodas <axel.bentodasilva@he-arc.ch>
2018-05-02 20:49:24 +0200

    Add Win Text

--- Assets/Script/EndFlag.cs
+++ Assets/Script/EndFlag.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndFlag : MonoBehaviour {
 
@@ -8,7 +9,7 @@ public class EndFlag : MonoBehaviour {
     {
         if(other.tag == "Player")
         {
-
+            GameManager.instance.Win();
         }
     }
 }
--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -4,7 +4,19 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
-    
+
+
+    public static GameManager instance = null;
+
+    public GameObject winText;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != null)
+            Destroy(gameObject);
+    }
 
     // Update is called once per frame
     void FixedUpdate() {
@@ -13,4 +25,13 @@ public class GameManager : MonoBehaviour {
         if (Input.GetKey(KeyCode.Escape))
             SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
     }
+
+    /// <summary>
+    /// Call when the player finish the level
+    /// </summary>
+    public void Win()
+    {
+        winText.SetActive(true);
+    }
+
 }
