6e4379f10272a4f0cc3715077179f64d0415f3c1
guillaum.noguera <guillaume.noguera@he-arc.ch>
2018-05-03 20:14:15 +0200

    musique

---
+++ Assets/MusicManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour {
+    public AudioClip intro;
+    public AudioClip loop;
+    private AudioSource aS;
+    void Start() {
+        DontDestroyOnLoad(this.gameObject);
+        aS = this.GetComponent<AudioSource>();
+        aS.loop = false;
+        aS.clip = intro;
+        aS.Play();
+    }
+    void Update() {
+        if (!aS.isPlaying) {
+            aS.clip = loop;
+            aS.loop = true;
+            aS.Play();
+        }
+    }
+}
